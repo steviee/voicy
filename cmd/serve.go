49c72cb30320dc23435a67f9b10e4c20b3b337a5
Stephan Eberle <stephan@deltacity.org>
2020-08-09 14:29:53 +0200

    Initial commit of branch

--- cmd/serve.go
+++ cmd/serve.go
@@ -22,13 +22,9 @@ import (
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "run the service on the local node",
+	Long: `server starts the tiny web-service to receive voice messages from the running website.
+	This is the initial version and only very bare.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		err := serve()
 		if err != nil {
